33bf136075b5192863511bbc9dfda63a4fdf3484
Pedro Duran <pedroluis.duran@externos-cl.cencosud.com>
2023-08-23 16:34:51 -0400

    jwt

--- src/cats/cats.module.ts
+++ src/cats/cats.module.ts
@@ -1,13 +1,15 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { BreedsModule } from 'src/breeds/breeds.module';
-import { BreedsService } from 'src/breeds/breeds.service';
+import { BreedsModule } from '../breeds/breeds.module';
+import { BreedsService } from '../breeds/breeds.service';
 import { CatsController } from './cats.controller';
 import { CatsService } from './cats.service';
 import { Cat } from './entities/cat.entity';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Cat]), BreedsModule],
+  imports: [TypeOrmModule.forFeature([Cat]), BreedsModule, AuthModule],
+
   controllers: [CatsController],
   providers: [CatsService, BreedsService],
 })
