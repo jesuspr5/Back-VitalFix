d0b22bbb3b219027de3a30b2dfd09de752b39f1b
jesus <pereirajesus35@gmail.com>
2023-08-28 19:49:58 -0400

    create comment

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,7 @@ import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
 import { RestaurantsModule } from './restaurants/restaurants.module';
+import { CommentsModule } from './comments/comments.module';
 
 @Module({
   imports: [
@@ -36,6 +37,7 @@ import { RestaurantsModule } from './restaurants/restaurants.module';
     UsersModule,
     AuthModule,
     RestaurantsModule,
+    CommentsModule,
   ],
   controllers: [],
   providers: [],
