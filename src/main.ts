21dcb5d953adff09e94cd1ad6879c64c764bf43e
Pedro Duran <pedroluis.duran@externos-cl.cencosud.com>
2023-09-08 20:16:59 -0400

    add name swagger

--- src/main.ts
+++ src/main.ts
@@ -17,8 +17,8 @@ async function bootstrap() {
   );
 
   const config = new DocumentBuilder()
-    .setTitle('Cats example')
-    .setDescription('The cats API description')
+    .setTitle('Restaurant social')
+    .setDescription('The restaurant social API description')
     .setVersion('1.0')
     .addBearerAuth()
     .build();
