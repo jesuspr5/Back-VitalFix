20297346a7868560a711315fd13b3d1d2a3f6fa6
Pedro Duran <pedroluis.duran@externos-cl.cencosud.com>
2023-09-19 02:16:15 -0400

    add method profile

--- src/users/dto/update-profile.dto.ts
+++ src/users/dto/update-profile.dto.ts
@@ -1,14 +1,12 @@
 import { Transform } from 'class-transformer';
-import { IsOptional, IsString, MinLength } from 'class-validator';
+import { IsString, MinLength } from 'class-validator';
 
 export class UpdateProfileDto {
-  @IsOptional()
   @Transform(({ value }) => value.trim())
   @IsString()
   @MinLength(1)
   name?: string;
 
-  @IsOptional()
   @Transform(({ value }) => value.trim())
   @IsString()
   @MinLength(1)
